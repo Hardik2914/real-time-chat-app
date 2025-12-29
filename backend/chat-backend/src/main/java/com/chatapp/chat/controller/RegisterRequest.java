f40a3ea421a457e74d2bc9a7075d61ce22f45f7a
Hardik2914 <hardikdixit29@gmail.com>
2025-12-29 23:15:39 +0530

    Add user registration API

---
+++ backend/chat-backend/src/main/java/com/chatapp/chat/controller/RegisterRequest.java
@@ -0,0 +1,32 @@
+package com.chatapp.chat.controller;
+
+public class RegisterRequest {
+
+    private String username;
+    private String password;
+    private String displayName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+}
---
+++ backend/chat-backend/src/main/java/com/chatapp/chat/service/UserService.java
@@ -0,0 +1,29 @@
+package com.chatapp.chat.service;
+
+import com.chatapp.chat.User;
+import com.chatapp.chat.repository.UserRepository;
+import jdk.jshell.spi.ExecutionControl;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+    public User registerUser(String username, String password, String displayName) {
+        if(userRepository.findByUsername(username).isPresent()){
+            throw new RuntimeException("Username already exists");
+        }
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setDisplayName(displayName);
+
+        return userRepository.save(user);
+    }
+}
